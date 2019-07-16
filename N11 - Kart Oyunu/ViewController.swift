d251c5c6d3fec44511973b89373b787e215be3cf
Fahri <mehmetfahribilici@gmail.com>
2019-07-16 15:21:29 +0300

    Initial Commit

---
+++ N11 - Kart Oyunu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  N11 - Kart Oyunu
+//
+//  Created by fahri.bilici on 16.07.2019.
+//  Copyright © 2019 fahri.bilici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
